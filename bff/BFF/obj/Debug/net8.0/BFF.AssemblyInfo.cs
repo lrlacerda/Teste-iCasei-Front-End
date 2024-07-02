97086ed3c23654b04533fb381b7fba4ccb872f3f
Lucas Lacerda <lribeirolacerda@gmail.com>
2024-07-02 13:55:35 -0300

    Teste ICasei: Front-End

--- bff/BFF/obj/Debug/net8.0/BFF.AssemblyInfo.cs
+++ bff/BFF/obj/Debug/net8.0/BFF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BFF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce8c7601f3f07615239e9f59eea7a608ac1b51c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87544dcecb46c11ed8c5db2a24ddb6118a184427")]
 [assembly: System.Reflection.AssemblyProductAttribute("BFF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BFF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
